d74530ff9e77fa45b4fac6e6f7ae6b6e062f1ce8
Ivin Joel Abraham <ivinjabraham@gmail.com>
2024-11-30 21:25:34 +0530

    send http response

--- main.s
+++ main.s
@@ -5,10 +5,12 @@
 def_msg startup_msg, "INFO: Starting web server...\n"
 def_msg write_err_msg, "ERROR: Failed to write to STDOUT.\n"
 def_msg socket_err_msg, "ERROR: Improper socket configuration.\n"
+def_msg response, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 38\r\n\n<html><body><h1>hello world</h1></body></html>"
 
 .section .bss
 
 .lcomm sock_fd, 8
+.lcomm conn_fd, 8
 .lcomm sockaddr, 64
 
 .section .text
@@ -33,8 +35,14 @@ _start:
         listen sock_fd(%rip), $num_connections
         check_error socket_err
 
+server_start:
         accept sock_fd(%rip)
         check_error socket_err
+
+        movq %rax, conn_fd(%rip)
+        write conn_fd(%rip), $response, $response_len
+        
+        jmp server_start
 
         # Exit successfully
         exit $success
